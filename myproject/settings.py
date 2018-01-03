cb07f90e6550a48f2f8d9df4e4966ca6ecb53f4d
roscas <sol.rosca@he-arc.ch>
2018-01-03 14:37:04 +0100

    registration

--- myproject/settings.py
+++ myproject/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'bootstrap4',
 
     'blog',
-    'accounts',
 ]
 
 MIDDLEWARE = [
--- myproject/urls.py
+++ myproject/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('blog.urls')),
-    path('', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
